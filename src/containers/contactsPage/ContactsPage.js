66d920b2f06876e1c19d6d902499b08c9b9c09d5
Joseph Giammona <joseph.e.giammona@gmail.com>
2023-03-26 17:19:29 -0400

    update useState and UseEffect

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -1,19 +1,44 @@
-import React from "react";
+import { React, useState, useEffect} from "react";
+import ContactForm from './ContactForm/ContactForm';
+import TileList from './TileList/TileList';
 
 export const ContactsPage = () => {
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [duplicate, setDuplicate] = useState(false);
+
   /*
   Define state variables for 
   contact info and duplicate check
   */
 
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    /*
-    Add contact info and clear data
-    if the contact name is not a duplicate
-    */
+    if(!diplicate){
+      addContact(name, phone, email);
+      setName('');
+      setPhone('');
+      setEmail('');
+    }
   };
 
+  useEffect(() => {
+    const nameIsDuplicate = () => {
+      const found = contacts.find((contact) => contact.name === name);
+      if (found !== undefined) {
+        return true;
+      }
+      return false;
+    };
+
+    if (nameIsDuplicate()) {
+      setDuplicate(true);
+    } else {
+      setDuplicate(false);
+    }
+  }, [name, contacts, duplicate]);
   /*
   Using hooks, check for contact name in the 
   contacts array variable in props
@@ -21,12 +46,25 @@ export const ContactsPage = () => {
 
   return (
     <div>
-      <section>
-        <h2>Add Contact</h2> 
+     <section>
+        <h2>
+          Add Contact
+          {duplicate ? " - Name Already Exists" : ""}
+        </h2>
+        <ContactForm
+          name={name}
+          setName={setName}
+          phone={phone}
+          setPhone={setPhone}
+          email={email}
+          setEmail={setEmail}
+          handleSubmit={handleSubmit}
+        />
       </section>
       <hr />
       <section>
         <h2>Contacts</h2>
+        <TileList tiles={contacts} />
       </section>
     </div>
   );
